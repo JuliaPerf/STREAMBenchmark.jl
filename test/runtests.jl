cf17148134e6ff0bf9ef104ed15c700b21756a4b
Carsten Bauer <crstnbr@gmail.com>
2021-03-28 19:57:56 +0200

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -101,6 +101,14 @@ use_less_memory = true
                     @warn "Compilation of original C STREAM benchmark failed."
                     println(e)
                 end
+                @test_throws ErrorException STREAMBenchmark.compile_original_STREAM(compiler=:carsten)
+            end
+        end
+
+        mktempdir() do tmpdir
+            cd(tmpdir) do
+                @test_logs (:warn,"Couldn't find source code \"stream/stream.c\". Have you run STREAMBenchmark.download_original_STREAM()?") STREAMBenchmark.compile_original_STREAM()
+                @test_logs (:warn,"Couldn't find executable \"stream/stream\". Have you run STREAMBenchmark.compile_original_STREAM()?") STREAMBenchmark.execute_original_STREAM()
             end
         end
     end
